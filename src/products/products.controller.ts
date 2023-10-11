760d561e15d433a56fad3ae49f48f685ff1515d6
Ilari Nicolas <ilarinico40@gmail.com>
2023-10-11 17:40:05 -0300

    añadida paginacion

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,27 +1,49 @@
-import { Body, Controller,Param ,Post,Get,ParseIntPipe, Delete, Put } from '@nestjs/common';
+import { Body, Controller,Param ,Post,Get,ParseIntPipe, Delete, Put, HttpException, NotFoundException, Query } from '@nestjs/common';
+
 
 import { CreateproductDto } from './dto/createProduct.dto';
 import { UpdateProductDto } from './dto/updateProduct.dto';
 import { Product } from './product.entity';
 import { ProductsService } from './products.service';
 
+
 @Controller('productos') //in the code i use the classes names in english but i specify the endpoint in spanish as peer the requirements
 export class ProductsController {
     constructor(private productService:ProductsService){
     }
 
-    @Post() //default endpoint products
-    createProduct(@Body()newProduct:CreateproductDto):Promise<Product>{
-        return this.productService.createProduct(newProduct);
+    @Post() 
+    createProduct(@Body()newProduct:CreateproductDto){
+        try{
+            const createdProduct=this.productService.createProduct(newProduct);
+            return createdProduct;
+        }catch (error) {
+            if(error instanceof HttpException)
+                throw new Error();
+        }
     }
+
     @Get()
-    getProducts():Promise<Product[]>{
-        return this.productService.getProducts();
+    getProducts(@Query('page')page:number,@Query('limit')limit:number){
+
+        if((limit&&!page)||(limit&&page)){
+            return this.productService.getPaginatedProducts(page,limit);
+        }
+            
+        else
+            return this.productService.getProducts();
     }
     @Get(':id')
-    getProduct(@Param('id',ParseIntPipe) id:number):Promise<Product>{
-        return this.productService.getProduct(id);
+    getProduct(@Param('id',ParseIntPipe) id:number){
+        try{
+            const product=this.productService.getProduct(id);
+            return product;
+        }
+        catch(err){
+            throw new Error("not found");
+        }
     }
+    
     @Delete(':id')
     deleteProduct(@Param('id',ParseIntPipe)id:number){
         return this.productService.deleteProduct(id);
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,HttpException,HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm'; //uses default repository
+import { paginate } from 'nestjs-typeorm-paginate';
 import { Repository } from 'typeorm';
+
 import { CreateproductDto } from './dto/createProduct.dto';
 import { UpdateProductDto } from './dto/updateProduct.dto';
 import {Product} from'./product.entity';
@@ -9,7 +11,24 @@ import {Product} from'./product.entity';
 export class ProductsService {
     constructor(@InjectRepository(Product)private productRepository:Repository<Product>){
     }
-    createProduct(product:CreateproductDto){
+    private findProductId(id:number){
+        return this.productRepository.findOne({where:{id}});
+    }
+    private returnNotFound(){
+        return new HttpException("Producto no encontrado",HttpStatus.NOT_FOUND); 
+    }
+
+    async getPaginatedProducts(page:number,limit:number){
+        const paginatedProducts= (await (paginate(this.productRepository,{page,limit}))).items;
+        return paginatedProducts;
+    }
+
+    async createProduct(product:CreateproductDto){
+
+        const findProduct=await this.productRepository.findOne({where:{nombre:product.nombre}});
+        if(findProduct){
+            return new HttpException('Producto con ese nombre ya existe',HttpStatus.CONFLICT);
+        }
         const newProduct=this.productRepository.create(product);
         return this.productRepository.save(newProduct);
     }
@@ -17,24 +36,26 @@ export class ProductsService {
         const findProducts= this.productRepository.find();
         return findProducts;
     }
-    getProduct(id:number){
-        const findProduct=this.productRepository.findOne({
-            where:{
-                id
-            }
-        });
-        return findProduct;
+    async getProduct(id:number){
+        const findProduct=await this.findProductId(id);
+        if(findProduct){
+            return findProduct;
+        }
+        return new HttpException("Producto no encontrado",HttpStatus.NOT_FOUND);
     }
     async deleteProduct(id:number){
-        const findProduct=await this.productRepository.findOne({
-            where:{
-                id
-            }
-        });
-        this.productRepository.remove(findProduct);
-        return findProduct;
+        const deleteResult=await this.productRepository.delete(id);
+        if(deleteResult.affected===0)
+            return this.returnNotFound();
+        return deleteResult;
     }
-    updateProduct(id:number,product:UpdateProductDto){
-        return this.productRepository.update({id},product);
+          
+    
+    async updateProduct(id:number,product:UpdateProductDto){
+        const findProduct=await this.findProductId(id);
+        if(findProduct)
+            return this.productRepository.update({id},product);
+        
+        return this.returnNotFound();
     }
 }
